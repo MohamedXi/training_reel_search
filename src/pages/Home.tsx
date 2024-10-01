7cb25531944508469527c7cc7ef5b48fdf971b47
Ismael MOHAMED <i.mohamed@groupe-creative.fr>
2024-10-01 09:32:38 +0200

    feat(step 1): adding the text 'Bienvenue sur Reel Search !'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-4 bg-background text-text-primary">
-      <h1 className="text-3xl font-bold text-center my-8">Welcome to the starter template</h1>
+      <h1 className="text-3xl font-bold text-center my-8">Bienvenue sur Reel Search !</h1>
     </div>
   );
 }
